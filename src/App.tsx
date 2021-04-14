670548b8476dbdf1b9cd509017c4cf5a16761318
Owais <precisework01@gmail.com>
2021-04-14 23:55:09 +0500

    update2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import LaunchList from './components/LaunchList';
+import LaunchProfile from './components/LaunchProfile';
 import './App.css';
 
 function App() {
+  
+  const [id, setId] = useState(42);
+  const handleIdChange = React.useCallback(newId => {
+    setId(newId);
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LaunchList handleIdChange={handleIdChange} />
+      <LaunchProfile id={id} />
     </div>
   );
 }
